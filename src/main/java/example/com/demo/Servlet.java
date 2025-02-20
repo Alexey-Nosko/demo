566faf920d79cbec7522e083dcaf3a46911dd71e
Alexey-Nosko <alexeynosko17@gmail.com>
2025-02-20 17:52:20 +0300

    version 2

--- src/main/java/example/com/demo/Servlet.java
+++ src/main/java/example/com/demo/Servlet.java
@@ -1,11 +1,13 @@
 package example.com.demo;
 
-import java.io.*;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class Servlet extends HttpServlet {
