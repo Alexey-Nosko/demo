493efe10cfa9e1042cfd7072381ba628fac43d5b
Alexey-Nosko <alexeynosko17@gmail.com>
2025-02-20 14:12:59 +0300

    Initial commit

---
+++ src/main/java/example/com/demo/Servlet.java
@@ -0,0 +1,22 @@
+package example.com.demo;
+
+import java.io.*;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class Servlet extends HttpServlet {
+    private String message;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        message = "Hello World";
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+}
