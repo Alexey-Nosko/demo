00829ac17bc556bfaa81ab3816be03e0b0a9e6af
Alexey-Nosko <alexeynosko17@gmail.com>
2025-02-20 18:03:06 +0300

    version 5

--- src/main/java/example/com/demo/Servlet.java
+++ src/main/java/example/com/demo/Servlet.java
@@ -14,7 +14,7 @@ public class Servlet extends HttpServlet {
     private String message;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        message = "Hello World 1";
+        message = "Hello World 2";
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
